08d7759eb20b7dcf08e14ec15df2e871a08de958
Ciaran De Groot <ciaran.deg@gmail.com>
2025-02-28 17:04:32 -0700

    Refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -5,20 +5,23 @@
     self,
     nixpkgs,
   }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
-    packages.x86_64-linux.tonka = pkgs.stdenv.mkDerivation {
-      pname = "tonka";
-      version = "0.0.1";
-      src = ./.;
-      nativeBuildInputs = [];
-      buildPhase = "";
-      installPhase = ''
-        mkdir -p $out
-        cp -r ./. $out/
-      '';
-    };
+    packages.${system} = {
+      tonka = pkgs.stdenv.mkDerivation {
+        pname = "tonka";
+        version = "0.0.1";
+        src = ./.;
+        nativeBuildInputs = [];
+        buildPhase = "";
+        installPhase = ''
+          mkdir -p $out
+          cp -r ./. $out/
+        '';
+      };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.tonka;
+      default = self.packages.${system}.tonka;
+    };
   };
 }
