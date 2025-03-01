082e67c6d29cb4f2a319fda94858f11cdb4b7f7d
Ciaran De Groot <ciaran.deg@gmail.com>
2025-02-28 18:30:05 -0700

    Move hello into separate function

--- lua/tonka.fnl
+++ lua/tonka.fnl
@@ -1,12 +1,14 @@
 (local tonka {})
 
+(fn tonka.hello [opts]
+  (if opts.name
+            (print "hello," opts.name)
+            (print "hello")))
+
 (fn tonka.setup [opts]
   (let [opts (or opts {})]
     (print opts)
     (vim.keymap.set "n" "<Leader>h"
-      (fn []
-        (if opts.name
-          (print "hello," opts.name)
-          (print "hello"))))))
+      (fn [] (tonka.hello opts)))))
 
 tonka
--- lua/tonka.lua
+++ lua/tonka.lua
@@ -1,15 +1,18 @@
 -- [nfnl] Compiled from lua/tonka.fnl by https://github.com/Olical/nfnl, do not edit.
 local tonka = {}
+tonka.hello = function(opts)
+  if opts.name then
+    return print("hello,", opts.name)
+  else
+    return print("hello")
+  end
+end
 tonka.setup = function(opts)
   local opts0 = (opts or {})
   print(opts0)
-  local function _1_()
-    if opts0.name then
-      return print("hello,", opts0.name)
-    else
-      return print("hello")
-    end
+  local function _2_()
+    return tonka.hello(opts0)
   end
-  return vim.keymap.set("n", "<Leader>h", _1_)
+  return vim.keymap.set("n", "<Leader>h", _2_)
 end
 return tonka
